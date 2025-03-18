c1a53ee01081a811096dac1a4a99548881f64112
Enock <oswereenock@gmail.com>
2025-03-18 14:46:57 +0300

    whitenoise

--- league/settings.py
+++ league/settings.py
@@ -42,7 +42,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'league.urls'
